96eedd932ddeac999bdd25230e07712eecb845bf
Prashant Navare <pnavare@texity.com>
2017-10-12 10:46:54 +0530

    UI Improvements...

--- app/src/main/java/com/navare/prashant/resortmanager/MainActivity.java
+++ app/src/main/java/com/navare/prashant/resortmanager/MainActivity.java
@@ -36,18 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private GridView mGridView;
     private AdView mAdView;
 
-    private InterstitialAd mInterstitialAdForReservations;
-    private InterstitialAd mInterstitialAdForTasks;
-    private InterstitialAd mInterstitialAdForReports;
-    private InterstitialAd mInterstitialAdForRooms;
-    private InterstitialAd mInterstitialAdForInventory;
-    private InterstitialAd mInterstitialAdForBackupRestore;
-    private IabHelper mHelper;
     private Activity mThisActivity;
-
-    static final String SKU_RESORT_MANAGER = "com.navare.prashant.resortmanager";
-    // (arbitrary) request code for the purchase flow
-    static final int PURCHASE_REQUEST = 10001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,9 +90,6 @@ public class MainActivity extends AppCompatActivity {
                     case 5:
                         onBackupRestoreClick();
                         break;
-                    case 6:
-                        onRemoveAdsClick();
-                        break;
                 }
             }
         });
@@ -135,13 +121,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initGridAdapater() {
-        int numButtons = 0;
-        if (ResortManagerApp.isAppPurchased()) {
-            numButtons = 6;
-        }
-        else {
-            numButtons = 7;
-        }
+        int numButtons = 6;
         String[]    tileTextArray = new String[numButtons];
         int[]       tileImageArray = new int[numButtons];
 
@@ -158,11 +138,6 @@ public class MainActivity extends AppCompatActivity {
         tileImageArray[3] = R.drawable.ic_inventory;
         tileImageArray[4] = R.drawable.ic_reports;
         tileImageArray[5] = R.drawable.ic_backup;
-
-        if (numButtons == 7) {
-            tileTextArray[6] = getString(R.string.remove_ads);
-            tileImageArray[6] = R.drawable.ic_remove_ads;
-        }
 
         NavigationGridAdapter adapter = new NavigationGridAdapter(this, tileTextArray, tileImageArray);
         mGridView.setAdapter(adapter);
@@ -187,99 +162,6 @@ public class MainActivity extends AppCompatActivity {
         mAdView.setVisibility(View.VISIBLE);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
-
-        // Reservations related interstitial ad
-        mInterstitialAdForReservations = new InterstitialAd(this);
-        mInterstitialAdForReservations.setAdUnitId(getString(R.string.interstitial_tasks_ad_unit_id));
-
-        mInterstitialAdForReservations.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitialForReservations();
-                onReservationsClick();
-            }
-        });
-
-        // Tasks related interstitial ad
-        mInterstitialAdForTasks = new InterstitialAd(this);
-        mInterstitialAdForTasks.setAdUnitId(getString(R.string.interstitial_tasks_ad_unit_id));
-
-        mInterstitialAdForTasks.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitialForTasks();
-                onTasksClick();
-            }
-        });
-
-        // Rooms related interstitial ad
-        mInterstitialAdForRooms = new InterstitialAd(this);
-        mInterstitialAdForRooms.setAdUnitId(getString(R.string.interstitial_reports_ad_unit_id));
-
-        mInterstitialAdForRooms.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitialForRooms();
-                onRoomsClick();
-            }
-        });
-
-        // Inventory related interstitial ad
-        mInterstitialAdForInventory = new InterstitialAd(this);
-        mInterstitialAdForInventory.setAdUnitId(getString(R.string.interstitial_inventory_ad_unit_id));
-
-        mInterstitialAdForInventory.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitialForInventory();
-                onInventoryClick();
-            }
-        });
-
-        // Reports related interstitial ad
-        mInterstitialAdForReports = new InterstitialAd(this);
-        mInterstitialAdForReports.setAdUnitId(getString(R.string.interstitial_tasks_ad_unit_id));
-
-        mInterstitialAdForReports.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitialForReports();
-                onReportsClick();
-            }
-        });
-
-        // Backup/Restore related interstitial ad
-        mInterstitialAdForBackupRestore = new InterstitialAd(this);
-        mInterstitialAdForBackupRestore.setAdUnitId(getString(R.string.interstitial_backuprestore_ad_unit_id));
-
-        mInterstitialAdForBackupRestore.setAdListener(new AdListener() {
-            @Override
-            public void onAdClosed() {
-                requestNewInterstitialForSetup();
-                onBackupRestoreClick();
-            }
-        });
-    }
-
-    private void doAdsReload() {
-        if (mInterstitialAdForReservations != null && !mInterstitialAdForReservations.isLoaded()) {
-            requestNewInterstitialForReservations();
-        }
-        if (mInterstitialAdForTasks != null && !mInterstitialAdForTasks.isLoaded()) {
-            requestNewInterstitialForTasks();
-        }
-        if (mInterstitialAdForReports != null && !mInterstitialAdForReports.isLoaded()) {
-            requestNewInterstitialForReports();
-        }
-        if (mInterstitialAdForRooms != null && !mInterstitialAdForRooms.isLoaded()) {
-            requestNewInterstitialForRooms();
-        }
-        if (mInterstitialAdForInventory != null && !mInterstitialAdForInventory.isLoaded()) {
-            requestNewInterstitialForInventory();
-        }
-        if (mInterstitialAdForBackupRestore != null && !mInterstitialAdForBackupRestore.isLoaded()) {
-            requestNewInterstitialForSetup();
-        }
     }
 
     // Called when leaving the activity
@@ -310,7 +192,6 @@ public class MainActivity extends AppCompatActivity {
             if (mAdView != null) {
                 mAdView.resume();
             }
-            doAdsReload();
         }
     }
 
@@ -322,212 +203,38 @@ public class MainActivity extends AppCompatActivity {
             mAdView.destroy();
             mAdView = null;
         }
-        if (mHelper != null) {
-            mHelper.disposeWhenFinished();
-            mHelper = null;
-        }
     }
 
-    private void requestNewInterstitialForReservations() {
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mInterstitialAdForReservations.loadAd(adRequest);
-    }
-
-    private void requestNewInterstitialForTasks() {
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mInterstitialAdForTasks.loadAd(adRequest);
-    }
-
-    private void requestNewInterstitialForReports() {
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mInterstitialAdForReports.loadAd(adRequest);
-    }
-
-    private void requestNewInterstitialForRooms() {
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mInterstitialAdForRooms.loadAd(adRequest);
-    }
-
-    private void requestNewInterstitialForInventory() {
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mInterstitialAdForInventory.loadAd(adRequest);
-    }
-
-    private void requestNewInterstitialForSetup() {
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mInterstitialAdForBackupRestore.loadAd(adRequest);
-    }
 
     public void onReservationsClick() {
-        if (mInterstitialAdForReservations != null && mInterstitialAdForReservations.isLoaded()) {
-            mInterstitialAdForReservations.show();
-        }
-        else {
-            startActivity(new Intent(this, ReservationsActivity.class));
-        }
+        startActivity(new Intent(this, ReservationsActivity.class));
     }
 
     public void onTasksClick()
     {
-        if (mInterstitialAdForTasks != null && mInterstitialAdForTasks.isLoaded()) {
-            mInterstitialAdForTasks.show();
-        }
-        else {
-            startActivity(new Intent(this, TaskListActivity.class));
-        }
+        startActivity(new Intent(this, TaskListActivity.class));
     }
 
     public void onRoomsClick() {
-        if (mInterstitialAdForRooms != null && mInterstitialAdForRooms.isLoaded()) {
-            mInterstitialAdForRooms.show();
-        }
-        else {
-            startActivity(new Intent(this, RoomListActivity.class));
-        }
+        startActivity(new Intent(this, RoomListActivity.class));
     }
 
     public void onInventoryClick() {
-        if (mInterstitialAdForInventory != null && mInterstitialAdForInventory.isLoaded()) {
-            mInterstitialAdForInventory.show();
-        }
-        else {
-            startActivity(new Intent(this, ItemListActivity.class));
-        }
+        startActivity(new Intent(this, ItemListActivity.class));
     }
 
     public void onReportsClick() {
-        if (mInterstitialAdForReports != null && mInterstitialAdForReports.isLoaded()) {
-            mInterstitialAdForReports.show();
-        }
-        else {
-            startActivity(new Intent(this, ReportsActivity.class));
-        }
+        startActivity(new Intent(this, ReportsActivity.class));
     }
 
     public void onBackupRestoreClick() {
-        if (mInterstitialAdForBackupRestore != null && mInterstitialAdForBackupRestore.isLoaded()) {
-            mInterstitialAdForBackupRestore.show();
-        }
-        else {
-            startActivity(new Intent(this, BackupRestoreActivity.class));
-        }
+        startActivity(new Intent(this, BackupRestoreActivity.class));
     }
 
     public void onSettingsClick() {
         startActivity(new Intent(this, SettingsActivity.class));
     }
 
-
-    public void onRemoveAdsClick() {
-
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setTitle("Remove Ads");
-        alertDialog.setMessage("Would you like to purchase  Hotel/Resort Manager and remove the ads?");
-        alertDialog.setIcon(R.mipmap.ic_resort_manager);
-        alertDialog.setPositiveButton("Purchase", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog,int which) {
-
-                initiatePurchase();
-            }
-        });
-
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        alertDialog.show();
-    }
-
-    private void initiatePurchase() {
-
-        String base64EncodedPublicKey = "com.navare.prashant.resortmanager";
-
-        mHelper = new IabHelper(this, base64EncodedPublicKey);
-        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
-            public void onIabSetupFinished(IabResult result) {
-                if (!result.isSuccess()) {
-                    // Oh noes, there was a problem.
-                    Log.d("initiatePurchase()", "Problem setting up In-app Billing: " + result);
-                    showPurchaseErrorAlert("There was a problem with the purchase. Please try again later. Thank you.");
-                    return;
-                }
-                // Have we been disposed of in the meantime? If so, quit.
-                if (mHelper == null) {
-                    showPurchaseErrorAlert("There was a problem with the purchase. Please try again later. Thank you.");
-                    return;
-                }
-                // Hooray, IAB is fully set up!
-                Log.d("initiatePurchase()", "Launching purchase flow...");
-                setWaitScreen(true);
-
-                String payload = "";
-                try {
-                    mHelper.launchPurchaseFlow(mThisActivity, SKU_RESORT_MANAGER, PURCHASE_REQUEST,
-                            mPurchaseFinishedListener, payload);
-                } catch (IabAsyncInProgressException e) {
-                    showPurchaseErrorAlert("Another purchase operation may be in progress. Please try again later.");
-                    setWaitScreen(false);
-                }
-            }
-        });
-    }
-
-    private void showPurchaseErrorAlert(String message) {
-        showPurchaseAlertInternal(message, true);
-    }
-
-    private void showPurchaseSuccessAlert(String message) {
-        showPurchaseAlertInternal(message, false);
-    }
-
-    private void showPurchaseAlertInternal(String message, boolean bError) {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        if (bError) {
-            alertDialog.setTitle("Purchase Error");
-            alertDialog.setIcon(R.drawable.ic_error);
-        }
-        else {
-            alertDialog.setTitle("Purchase Successful");
-            alertDialog.setIcon(R.drawable.ic_success);
-        }
-        alertDialog.setMessage(message);
-        alertDialog.setNeutralButton("OK", null);
-        alertDialog.create().show();
-    }
-
-    // Callback for when a purchase is finished
-    IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
-        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
-            Log.d("initiatePurchase()", "Purchase finished: " + result + ", purchase: " + purchase);
-
-            // if we were disposed of in the meantime, quit.
-            if (mHelper == null)
-                return;
-
-            if (result.isFailure()) {
-                showPurchaseErrorAlert("There was an error while completing the purchase. Please try and again later.");
-                setWaitScreen(false);
-                return;
-            }
-
-            Log.d("initiatePurchase()", "Purchase successful.");
-
-            if (purchase.getSku().equals(SKU_RESORT_MANAGER)) {
-                // bought the  Resort Manager app!
-                Log.d("initiatePurchase()", "Purchase is premium upgrade. Congratulating user.");
-                showPurchaseSuccessAlert("Thank you for for the purchase.");
-                ResortManagerApp.setPurchaseValue(ResortManagerApp.APP_PURCHASED);
-                removeAdStuff();
-                setWaitScreen(false);
-            }
-        }
-    };
-
-    void setWaitScreen(boolean set) {
-        // TODO: Implement purchase Wait screen
-    }
 
     // TODO: Revisit all alert dialogs for proper messaging (e.g. Adults should not empty should read Adults field should not be empty)
 }
--- app/src/main/java/com/navare/prashant/resortmanager/ResortManagerApp.java
+++ app/src/main/java/com/navare/prashant/resortmanager/ResortManagerApp.java
@@ -1,12 +1,17 @@
 package com.navare.prashant.resortmanager;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Application;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
+import com.navare.prashant.resortmanager.InAppBilling.IabHelper;
+import com.navare.prashant.resortmanager.InAppBilling.IabResult;
+import com.navare.prashant.resortmanager.InAppBilling.Purchase;
 import com.navare.prashant.resortmanager.util.ComputeNewTasksAlarmReceiver;
 
 /**
@@ -25,6 +30,7 @@ public class ResortManagerApp extends Application {
     public static String sPrefTaskCount = "TaskCount";
     public static String sPrefItemCount = "ItemCount";
     public static String sPrefRoomCount = "RoomCount";
+    public static String sPrefTotalNewReservationCount = "TotalNewReservationCount";
     public static String sPrefNewReservationCount = "NewReservationCount";
     public static String sPrefCheckedInReservationCount = "CheckedInReservationCount";
     public static String sPrefHistoricalReservationCount = "HistoricalReservationCount";
@@ -34,6 +40,9 @@ public class ResortManagerApp extends Application {
 
     private static SharedPreferences mPrefs;
     private static SharedPreferences.Editor mEditor;
+
+    // purchase related
+    static private IabHelper mHelper;
 
     @Override
     public void onCreate() {
@@ -55,6 +64,14 @@ public class ResortManagerApp extends Application {
             alarmReceiver.setAlarm(mAppContext, true);
         }
     }
+
+    // Max free rooms before requiring a purchase
+    private static int mMaxFreeRooms = 1;
+    static public int getMaxFreeRooms() { return mMaxFreeRooms;}
+
+    // Max free reservations before requiring a purchase
+    private static int mMaxFreeReservations = 1;
+    static public int getMaxFreeReservations() { return mMaxFreeReservations;}
 
     // Task
     static public void incrementTaskCount() {
@@ -116,7 +133,23 @@ public class ResortManagerApp extends Application {
         return mPrefs.getLong(sPrefRoomCount, 0);
     }
 
-    // New reservations
+    // Total New reservations
+    static public void incrementTotalNewReservationCount() {
+        changeTotalNewReservationCount(1);
+    }
+
+    static private void changeTotalNewReservationCount(long numReservations) {
+        long reservationCount = mPrefs.getLong(sPrefTotalNewReservationCount, 0);
+        reservationCount = reservationCount + numReservations;
+        mEditor.putLong(sPrefTotalNewReservationCount, reservationCount);
+        mEditor.commit();
+    }
+
+    static public long getTotalNewReservationsCount() {
+        return mPrefs.getLong(sPrefTotalNewReservationCount, 0);
+    }
+
+    // current New reservations
     static public void incrementNewReservationCount() {
         changeNewReservationCount(1);
     }
@@ -174,17 +207,6 @@ public class ResortManagerApp extends Application {
         return mPrefs.getLong(sPrefHistoricalReservationCount, 0);
     }
 
-    // purchase value
-    static public void setPurchaseValue(long purchaseValue) {
-        mEditor.putLong(sPrefPurchaseValue, purchaseValue);
-        mEditor.commit();
-    }
-
-    static public boolean isAppPurchased() {
-        long purchaseValue = mPrefs.getLong(sPrefPurchaseValue, 0);
-        return purchaseValue == APP_PURCHASED;
-    }
-
     // Organization name
     static public String getOrgName() {
         return mPrefs.getString(sPrefOrganizationName, "");
@@ -208,4 +230,141 @@ public class ResortManagerApp extends Application {
         alertDialog.show();
     }
 
+    // purchase related
+    static public void setPurchaseValue(long purchaseValue) {
+        mEditor.putLong(sPrefPurchaseValue, purchaseValue);
+        mEditor.commit();
+    }
+
+    static public boolean isAppPurchased() {
+        long purchaseValue = mPrefs.getLong(sPrefPurchaseValue, 0);
+        return purchaseValue == APP_PURCHASED;
+    }
+
+    public interface PurchaseListener {
+        void onPurchaseCompleted();
+    }
+
+    private static Activity mPurchaseActivity;
+
+    static public void promptForPurchase(final Activity callingActivity, String purchaseMessage) {
+
+        if (callingActivity instanceof PurchaseListener) {
+            mPurchaseActivity = callingActivity;
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(mAppContext);
+            alertDialog.setTitle("Purchase Hotel/Resort Manager");
+            alertDialog.setMessage(purchaseMessage);
+            alertDialog.setIcon(R.mipmap.ic_resort_manager);
+            alertDialog.setPositiveButton("Purchase", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog,int which) {
+
+                    initiatePurchase(callingActivity);
+                }
+            });
+
+            alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+            alertDialog.show();
+        }
+    }
+
+    private static final String SKU_RESORT_MANAGER = "com.navare.prashant.resortmanager";
+    // (arbitrary) request code for the purchase flow
+    private static final int PURCHASE_REQUEST = 10001;
+
+    static private void initiatePurchase(final Activity callingActivity) {
+
+        String base64EncodedPublicKey = "com.navare.prashant.resortmanager";
+
+        mHelper = new IabHelper(mAppContext, base64EncodedPublicKey);
+        mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
+            public void onIabSetupFinished(IabResult result) {
+                if (!result.isSuccess()) {
+                    // Oh noes, there was a problem.
+                    Log.d("initiatePurchase()", "Problem setting up In-app Billing: " + result);
+                    showPurchaseErrorAlert("There was a problem with the purchase. Please try again later. Thank you.");
+                    return;
+                }
+                // Have we been disposed of in the meantime? If so, quit.
+                if (mHelper == null) {
+                    showPurchaseErrorAlert("There was a problem with the purchase. Please try again later. Thank you.");
+                    return;
+                }
+                // Hooray, IAB is fully set up!
+                Log.d("initiatePurchase()", "Launching purchase flow...");
+                setWaitScreen(true);
+
+                String payload = "";
+                try {
+                    mHelper.launchPurchaseFlow(callingActivity, SKU_RESORT_MANAGER, PURCHASE_REQUEST,
+                            mPurchaseFinishedListener, payload);
+                }
+                catch (IabHelper.IabAsyncInProgressException e) {
+                    showPurchaseErrorAlert("Another purchase operation may be in progress. Please try again later.");
+                    setWaitScreen(false);
+                }
+            }
+        });
+    }
+
+    static private void showPurchaseErrorAlert(String message) {
+        showPurchaseAlertInternal(message, true);
+    }
+
+    static private void showPurchaseSuccessAlert(String message) {
+        showPurchaseAlertInternal(message, false);
+    }
+
+    static private void showPurchaseAlertInternal(String message, boolean bError) {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mAppContext);
+        if (bError) {
+            alertDialog.setTitle("Purchase Error");
+            alertDialog.setIcon(R.drawable.ic_error);
+        }
+        else {
+            alertDialog.setTitle("Purchase Successful");
+            alertDialog.setIcon(R.drawable.ic_success);
+        }
+        alertDialog.setMessage(message);
+        alertDialog.setNeutralButton("OK", null);
+        alertDialog.create().show();
+    }
+
+    // Callback for when a purchase is finished
+    private static IabHelper.OnIabPurchaseFinishedListener mPurchaseFinishedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        public void onIabPurchaseFinished(IabResult result, Purchase purchase) {
+            Log.d("initiatePurchase()", "Purchase finished: " + result + ", purchase: " + purchase);
+
+            // if we were disposed of in the meantime, quit.
+            if (mHelper == null)
+                return;
+
+            if (result.isFailure()) {
+                showPurchaseErrorAlert("There was an error while completing the purchase. Please try and again later.");
+                setWaitScreen(false);
+                return;
+            }
+
+            Log.d("initiatePurchase()", "Purchase successful.");
+
+            if (purchase.getSku().equals(SKU_RESORT_MANAGER)) {
+                // bought the  Resort Manager app!
+                Log.d("initiatePurchase()", "Purchase is premium upgrade. Congratulating user.");
+                showPurchaseSuccessAlert("Thank you for for the purchase.");
+                ResortManagerApp.setPurchaseValue(ResortManagerApp.APP_PURCHASED);
+                if (mPurchaseActivity != null) {
+                    ((PurchaseListener) mPurchaseActivity).onPurchaseCompleted();
+                }
+                setWaitScreen(false);
+            }
+        }
+    };
+
+    static void setWaitScreen(boolean set) {
+        // TODO: Implement purchase Wait screen
+    }
 }
